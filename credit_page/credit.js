94974d982cd9c7cf98582de626dbe8f4ad85c9bc
Aleksei <kilroygood@gmail.com>
2020-09-15 22:28:50 +0300

    done

--- credit_page/credit.js
+++ credit_page/credit.js
@@ -1,65 +1,322 @@
 const mockData = [
-	{"logo": "", 
-	"reiting":"4.6",
-	"summCredit": "",
-	"percent":"3",
+	{"logo": "images/credit7.png", 
+	"reiting":4.6,
+	"summCredit": 100,
+	"percent":0.01,
 	"return": "",
-	"depositValue":"",
-	"firstCredit": "",
-	"lastCredit": "",
-	"time": "",
+	"isPopular":true,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 3,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
 	},
-	{"logo": "", 
-	"reiting":"3",
-    "summCredit": "",
-	"percent":"",
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":3,
+    "summCredit": 100,
+	"percent":0.05,
 	"return": "",
-	"depositValue":"",
-	"firstCredit": "",
-	"lastCredit": "",
-	"time": "",
+	"isPopular":false,
+	"Allusers": true,
+	"fastCredit": false,
+	"time": 3,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
 	},
-	{"logo": "", 
-	"reiting":"5",
-	"summCredit": "",
-	"percent":"",
+	{"logo": "images/mycredit.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
 	"return": "",
-	"depositValue":"",
-	"firstCredit": "",
-	"lastCredit": "",
-	"time": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": true,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
+	},
+	{"logo": "images/e4a3b2325134ba156bbef4f9b1fe7e02.png", 
+	"reiting":5,
+	"summCredit": 100,
+	"percent":0.03,
+	"return": "",
+	"isPopular":false,
+	"Allusers": false,
+	"fastCredit": false,
+	"time": 12,
+	"firstCr": "500 - 7 000",
+	"scndCr": "500 - 5 000",
+	"srok": "2 - 30 дней",
 	}
 ]
 
-function createSeries(datarecord, summ = 1) {
+function renderTemplaite(props) {
+	console.log(props)
+	const {data, summCredit ,time } = props
+	console.log(summCredit)
+
+	const price = summCredit ? summCredit : data.summCredit
+	const month = time ? time : data.time
+
+	const SummAmmount = (price*data.percent) + (month*2) + price;
+	const persentCalculate = (price*data.percent) + (month*2) 
+
+	console.log(typeof price,price ,"price")
+	
+	
+
 	return `
-	<div>
-	<h2 class="a-Series_Title">${parseInt(datarecord.reiting)*summ}</h2>
-	  <p class="a-Series_Description">
-		<span class="a-Series_DescriptionHeader">Description: </span>${datarecord.percent}
-	  </p>
-	  <div class="a-EpisodeBlock">
-		<h4 class="a-EpisodeBlock_Title">First episodes</h4>
-	  </div>
-	</div>  
+	<div class="box1" >
+
+	${data.isPopular ? "<div class='isPopularDiv'><p class='isPopularP'>98% одобрений</p></div>" : ""}
+	${data.Allusers ? "<div class='AllusersDiv'><p class='AllusersP'>Лучшее предложение</p></div>" : ""}
+	${data.fastCredit ? "<div class='fastCreditDiv'><p class='fastCreditP'>Кредит за 15 минут</p></div>" : ""}
+
+
+		<div class="box">
+		<div class="box_logo_block">
+			<div class="box_logo_block_img">
+				<img src="${data.logo}" />
+			</div>
+			<div class="box_logo_stars_block">
+				<div class="stars">
+					<div class="star">
+						<i class="far fa-star" style=' font-size: 16px'></i>
+					</div>
+					<div class="star">
+					<i class="far fa-star" style=' font-size: 16px'></i>
+					</div>
+					<div class="star">
+					<i class="far fa-star" style=' font-size: 16px'></i>
+					</div>
+					<div class="star">
+					<i class="far fa-star" style=' font-size: 16px'></i>
+					</div>
+					<div class="star">
+					<i class="far fa-star" style=' font-size: 16px'></i>
+					</div>
+				</div>
+				<p class="ball">(${data.reiting})</p>
+			</div>
+		</div>
+
+		<div class="main_part">
+			<div class="main_part_top">
+				<div class="main_part_top_summ">
+					<p class="main_part_top_summ_txt">Сумма кредита</p>
+					<div class="main_part_top_summ_bl">
+						<p class="main_part_top_summ_numb" id="">${price} <p class="main_part_top_summ_numb_grn">грн</p></p>
+					</div>
+				</div>
+				<div class="main_part_top_summ">
+					<p class="main_part_top_prozents_txt">Проценты</p>
+					<div class="main_part_top_summ_bl2">
+						<p class="main_part_top_summ_numb" id=""> ${persentCalculate}</p>
+						<p class="main_part_top_summ_numb_grn">грн</p>
+					</div>
+				</div>
+				<div class="main_part_top_summ">
+					<p class="main_part_top_back_txt">Сумма кредита</p>
+					<div class="main_part_top_summ_bl3">
+						<p class="main_part_top_summ_numb" id="">${SummAmmount}</p>
+						<p class="main_part_top_summ_numb_grn">грн</p>
+					</div>
+				</div>
+			</div>
+				<div class="main_part_line"></div>
+			<div class="main_part_bottom">
+				<div class="main_part_bottom_summ">
+					<p class="main_part_bottom_summ_txt">Первый кредит</p>
+					<p class="main_part_bottom_summ_numb">${data.firstCr}</p>
+				</div>
+				<div class="main_part_bottom_summ">
+					<p class="main_part_bottom_summ_txt">Повторный кредит</p>
+					<p class="main_part_bottom_summ_numb">${data.scndCr}</p>
+				</div>
+				<div class="main_part_bottom_summ">
+					<p class="main_part_bottom_summ_txt">Срок</p>
+					<p class="main_part_bottom_summ_numb">${data.srok}</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="last_block">
+			<div class="last_block_proz">
+				<div class="last_block_proz_box">
+					<p class="last_block_proz_txt">${data.percent}</p>
+				</div>
+				<div class="last_block_txt_box">
+					<p class="last_block_txt">на первый кредит</p>
+				</div>
+			</div>
+			<div class="bnt">
+				<p class='bnt_txt'>получить кредит</p>
+			</div>
+		</div>
+	</div>
+</div>  
 	`;
   }
-	
-	document.getElementById("btn").addEventListener("click", () => {
-		renderCreditItems(mockData, 2)
-		console.log("click")
+	const summCredit = document.getElementById("summSl");
+	const time =document.getElementById("timeSl");
+
+	summCredit.addEventListener("change", (e) => {
+		const s = parseInt( e.target.value )
+		const t = parseInt( time.value )
+		renderCreditItems({data : mockData, summCredit: s, time: t})
+		console.log(typeof e.target.value)
+	})
+	time.addEventListener("change", (e) => {
+		const t = parseInt( e.target.value )
+		const s = parseInt( summCredit.value )
+		renderCreditItems({data : mockData, time: t , summCredit: s })
+		console.log(typeof e.target.value)
 	})
 
-function renderCreditItems(mockData, summ, month) {
-	if(mockData.length === 0) return false
+	
+	
+function renderCreditItems(props) {
+	const { data, summCredit ,time } = props
+	console.log(props)
+	if(data.length === 0) return false
 	const creditItems = document.getElementById('creditItems')
-	const templeit = mockData.map(element => {
-	return createSeries(element, summ)
+	const templeit = data.map(element => {
+	return renderTemplaite({data: element, summCredit:summCredit, time:time })
 	}).join("")	
 	console.log(typeof creditItems)
 	creditItems.innerHTML = templeit
-	console.log(templeit)
 }
-renderCreditItems(mockData);
-
-
+renderCreditItems({data : mockData});
